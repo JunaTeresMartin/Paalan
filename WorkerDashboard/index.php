4c81478b3d02376d7a20cd755933ffe92a3f5428
JunaTeresMartin <83876430+JunaTeresMartin@users.noreply.github.com>
2023-07-31 10:50:16 +0530

    Add files via upload

---
+++ WorkerDashboard/index.php
@@ -0,0 +1,194 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['valid'])) {
+    header("Location: login.php");
+    exit;
+}
+
+include("../WorkerDashboard/form3/connect.php");
+
+$workerId = $_SESSION['valid'];
+
+// Fetch the anganwadi worker name based on the worker id
+$sql = "SELECT aww_name FROM anganwadi WHERE worker_id = '$workerId'";
+$result = mysqli_query($con, $sql);
+$row = mysqli_fetch_assoc($result);
+$anganwadiWorkerName = $row['aww_name'];
+
+
+// Fetch the panchayat name based on the worker id
+$sql = "SELECT panchayat_name FROM anganwadi WHERE worker_id = '$workerId'";
+$result = mysqli_query($con, $sql);
+$row = mysqli_fetch_assoc($result);
+$panchayatName = $row['panchayat_name'];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Anganwadi Worker Dashboard</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+</head>
+<body>
+  <header>
+    <div class="top-section">
+      <div class="site-name">
+        <a href="../LandingPage/index1.php" style="text-decoration: none;" ><h1 style="color: white;">PAALAN</h1></a>
+        <h2 class="dashboard">Dashboard</h2>
+        <h2 class="dashboard">Hi <?php echo $anganwadiWorkerName; ?></h2>
+      </div>
+
+      <div class="left-section">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a id="messages-button" href="../AdminDashboard/registration/msg.php" class="icon-a"><i class="bi bi-chat"></i>&nbsp;&nbsp;Messages</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
+
+
+  <div class="sidebar">
+    <ul>
+      <li><a href="#visualization" class="icon-a"><i class="bi bi-graph-up"></i>&nbsp;&nbsp;Charts</a></li>
+      <li><a href="#bmi" class="icon-a"><i class="bi bi-table"></i>&nbsp;&nbsp;BMI</a></li>
+      <li><a href="../AdminDashboard/registration/stock.php" class="icon-a"><i class="bi bi-table"></i>&nbsp;&nbsp;Stock Request</a></li>
+      <li><a href="feedback.php" class="icon-a"><i class="bi bi-chat-left-text"></i>&nbsp;&nbsp;Feedback</a></li>
+      <li><a href="logout.php" class="icon-a"><i class="bi bi-person-circle"></i>&nbsp;&nbsp;Logout</a></li>
+      <li ><a href="help.html" class="icon-a"><i class="bi bi-info-circle"></i>&nbsp;&nbsp;Help</a></li>
+    </ul>
+  </div>
+ 
+
+
+
+  <h1 id="dashboard-title">Welcome to the <?php echo $panchayatName; ?><span> Anganwadi Worker Dashboard</span></h1>
+  <p id="welcome-para">Your dedicated platform for managing child welfare programs<br>and empowering young minds.</p>
+  
+  
+
+  <div class="container">
+    <div class="box" onclick="window.location.href='form1/form1.html';">
+      <div class="image-section">
+        <img src="motherChild2.jpg" alt="Image 1">
+      </div>
+      <div class="description">
+        <h3>CHILD REGISTRATION</h3>
+        <p>Click to Register</p>
+      </div>
+    </div>
+    <div class="box" onclick="window.location.href='form2/form2.html';">
+      <div class="image-section">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPSH7JSJFyx67kbyZS6DdwmoVfMDYobHNGkA&usqp=CAU" alt="Image 2">
+      </div>
+      <div class="description">
+        <h3>PREGNANT WOMEN REGISTRATION</h3>
+        <p>Click to Register</p>
+      </div>
+    </div>
+    <div class="box" onclick="window.location.href='form3/form3.html';">
+      <div class="image-section">
+        <img src="motherChild1.jpg" alt="Image 3">
+      </div>
+      <div class="description">
+        <h3>UPDATE DETAILS</h3>
+        <p>Click to Update</p>
+      </div>
+    </div>
+  </div>
+  
+  <!-- data visualization -->
+
+  <div class="graph-container" section id="visualization">
+    <div class="graph-box">
+      <div class="canvas-container">
+        <canvas id="lineChart"></canvas>
+      </div>
+    </div>
+    <div class="graph-box">
+      <div class="canvas-container">
+        <canvas id="barChart"></canvas>
+      </div>
+    </div>
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <script src="chart.js"></script>
+
+  <div class="form-container"section id="bmi">
+  <form class="bmi">
+    <h3>Child BMI Result</h3>
+    <table class="table table-striped table-dark table-hover" width=80%>
+      <thead class="table-primary">
+        <tr>
+          <th>Child Name</th>
+          <th>Age</th>
+          <th>BMI Percentage</th>
+          <th>Comment</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "paalan";
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        // Check connection
+        if ($conn == false) {
+            die("connection error");
+        }
+        // Fetch data from the 'child_details' table
+        $query = "SELECT * FROM child_details";
+        $result = mysqli_query($conn, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+          $childName = $row['child_name'];
+          $childAge = $row['child_age'];
+          $height = $row['height'];
+          $weight = $row['weight'];
+
+          // Skip children with missing height or weight values
+          if (empty($height) || empty($weight)) {
+            continue;
+          }
+
+          $bmiPercentage = ($weight / ($height * $height * 0.0001));
+          $comment = '';
+
+          // Determine the comment based on the BMI percentage value
+          if ($bmiPercentage < 5) {
+            $comment = 'Underweight';
+          } elseif ($bmiPercentage >= 5 && $bmiPercentage < 85) {
+            $comment = 'Healthy weight';
+          } elseif ($bmiPercentage >= 85 && $bmiPercentage < 95) {
+            $comment = 'Overweight';
+          } elseif ($bmiPercentage >= 95) {
+            $comment = 'Obese';
+          }
+          ?>
+          <tr>
+            <td><?php echo $childName; ?></td>
+            <td><?php echo $childAge; ?></td>
+            <td><?php echo $bmiPercentage; ?></td>
+            <td><?php echo $comment; ?></td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </form>
+</div>
+
+
+</body>
+</html> 
+
+
---
+++ WorkerDashboard/login.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    include("../WorkerDashboard/form3/connect.php");
+
+    $username = mysqli_real_escape_string($con, $_POST['username']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+
+    $result = mysqli_query($con, "SELECT * FROM worker_credentials WHERE username='$username'") or die("Select Error: " . mysqli_error($con));
+    $row = mysqli_fetch_assoc($result);
+
+    if ($row && $row['password'] === $password) {
+        $_SESSION['valid'] = $row['worker_id'];
+        $_SESSION['username'] = $row['Username'];
+
+       // Fetch worker_id from worker_credentials table
+       $workerId = $row['worker_id'];
+
+    //    // Fetch panchayat_name from anganwadi table using worker_id
+    //    $query = "SELECT panchayat_name FROM anganwadi WHERE worker_id='$workerId'";
+    //    $result = mysqli_query($con, $query);
+    //    $row = mysqli_fetch_assoc($result);
+
+
+    //    $_SESSION['panchayat_name'] = $row['panchayat_name'];
+
+
+        header("Location: index.php");
+        exit;
+    } else {
+        $error = "Wrong Username or Password.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Login</title>
+</head>
+
+<body>
+    <a href="../LandingPage/index1.php"><img src="icon.jpg" alt="PAALAN Icon" style="width: 24px; height: 24px; position: absolute; top: 10px; left: 10px;"></a>
+
+    <div class="container">
+        <div class="box form-box">
+            <?php if (isset($error)) : ?>
+                <div class='message'>
+                    <p><?php echo $error; ?></p>
+                </div><br>
+                <a href='login.php'><button class='btn'>Go Back</button></a>
+            <?php else : ?>
+                <header>Login</header>
+                <form action="" method="post">
+                    <div class="field input">
+                        <label for="username">username</label>
+                        <input type="text" name="username" id="username" required>
+                    </div>
+                    <div class="field input">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" required>
+                    </div>
+                    <div class="field">
+                        <input type="submit" class="btn" name="submit" value="Login" required>
+                    </div>
+                    <!-- <div class="links">
+                        Don't have an account? <a href="registration.php">Sign Up</a>
+                    </div> -->
+                </form>
+            <?php endif; ?>
+        </div>
+    </div>
+</body>
+
+</html>
